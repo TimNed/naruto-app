a6522abac35e073189365937b8439eb8d83ea0dc
Jessica <jessicarolinefr@gmail.com>
2023-05-01 14:45:35 +0200

    fix infinit loop

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
-import { Card } from './components/card';
-import './App.css';
-import { NarutoData } from './service';
-import {useEffect, useState} from 'react';
+import { Card } from "./components/card";
+import "./App.css";
+import { NarutoData } from "./service";
+import { useEffect, useState } from "react";
 
 function App() {
   const [actors, setCharacters] = useState([]);
+
   useEffect(() => {
     retrieveNaruto();
-  })
-  const retrieveNaruto = async() => {
-   await NarutoData() 
-    .then((response) => {
+  }, []);
+
+  const retrieveNaruto = async () => {
+    await NarutoData().then((response) => {
       setCharacters(response.data.characters);
-      
-    })
-  }
-  console.log(actors);
-  return (
-    actors.map(item => {
-      <Card
-        image={item.images[0]}
-        characterName={item.name}
-      />
-    })
-  
-  );
+    });
+  };
+
+  return actors.map((item) => {
+    return <Card image={item?.images[0]} characterName={item?.name} />;
+  });
 }
 
 export default App;
